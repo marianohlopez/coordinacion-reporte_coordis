f921344416d8fda4ee1fe6e5473f2a0c8362db97
Mariano Lopez <ml.3012@gmail.com>
2026-09-14 11:50:40 -0300

    add informes & seguimientos

--- extract.py
+++ extract.py
@@ -3,7 +3,8 @@ def extract_coordis(cursor):
     SELECT 
       DISTINCT p.prestacion_coordi, 
       CONCAT(p.coordi_apellido, ', ', p.coordi_nombre) AS coordinadora,
-      c.coordi_mail
+      c.coordi_mail,
+      c.user_id
     FROM 
       v_prestaciones p
     JOIN v_coordinadores c 
@@ -54,3 +55,114 @@ def extract_pas(cursor, coordi_id):
   cursor.execute(query)
   return cursor.fetchall()
 
+def extract_informes(cursor, coord_id):
+  query = """ 
+    SELECT 
+      CONCAT(c.coordi_apellido, ', ', c.coordi_nombre) AS nombre_coordi,
+      CONCAT(p.alumno_apellido, ', ', p.alumno_nombre) AS nombre_alumno,
+      p.alumno_dni,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe Inicial - ADMISIÓN" THEN i.alumnoinforme_id END) AS inf_admision,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Conformidad de PA" THEN i.alumnoinforme_id END) AS conf_pa,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe social de seguimiento" THEN i.alumnoinforme_id END) AS inf_social,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe Mensual" THEN i.alumnoinforme_id END) AS inf_mensual,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe Diagnóstico" THEN i.alumnoinforme_id END) AS inf_diagnostico,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe Medio" THEN i.alumnoinforme_id END) AS inf_medio,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "PAC" THEN i.alumnoinforme_id END) AS pac,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "OTRO" THEN i.alumnoinforme_id END) AS otro,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "AA" THEN i.alumnoinforme_id END) AS aa,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "PPI / PI" THEN i.alumnoinforme_id END) AS ppi,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe Final" THEN i.alumnoinforme_id END) AS inf_final,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Conformidad familia" THEN i.alumnoinforme_id END) AS conf_familia,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe escolar" THEN i.alumnoinforme_id END) AS inf_escolar,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Informe terapéutico ext." THEN i.alumnoinforme_id END) AS inf_ter_ext,
+      COUNT(DISTINCT CASE WHEN i.informecat_nombre = "Plan de trabajo - Coordinacion" THEN i.alumnoinforme_id END) AS plan_coordi
+    FROM v_prestaciones p
+    JOIN v_coordinadores c
+        ON p.prestacion_coordi = c.coordi_id
+    LEFT JOIN v_informes i 
+        ON p.alumno_id = i.alumno_id 
+        AND (
+            i.alumnoinforme_anio = '2026'
+            OR i.informecat_nombre = "Informe Inicial - ADMISIÓN"
+        )
+    WHERE
+        p.prestipo_nombre_corto != 'TERAPIAS'
+        AND p.prestacion_estado IN (0, 1)
+        AND p.prestacion_anio IN (2026, 2027)
+        AND p.prestacion_coordi = %s
+    GROUP BY p.alumno_id, p.alumno_dni
+    ORDER BY nombre_coordi;
+    """
+  cursor.execute(query, (coord_id, ))
+  return cursor.fetchall()
+
+def extract_seguim(cursor, coord_id):
+  query = """ 
+    SELECT 
+      usuario_carga_nombre,
+      s.segalum_rol_carga,
+      s.segalum_prestacion,
+      p.prestipo_nombre_corto,
+      CONCAT(a.alumno_apellido, ', ', a.alumno_nombre) AS nombre_alumno,
+      s.segalum_mesanio,
+      DATE_FORMAT(s.segalum_fec_carga, '%d-%m-%Y') AS fec_carga,
+      s.segcat_nombre
+    FROM
+      v_seguimientos s
+    LEFT JOIN v_prestaciones p
+      ON s.segalum_prestacion = p.prestacion_id
+    LEFT JOIN v_alumnos a
+      ON s.segalum_alumno = a.alumno_id
+    WHERE
+      (p.prestacion_estado IN (0, 1) OR p.prestacion_estado IS NULL)
+      AND YEAR(s.segalum_fec_carga) = 2026 
+      AND (p.prestacion_anio IN (2026, 2027) OR p.prestacion_anio IS NULL)
+      AND p.prestacion_coordi = %s
+    ORDER BY usuario_carga_nombre
+  """
+  cursor.execute(query, (coord_id, ))
+  return cursor.fetchall()
+
+def extract_seguim_mes(cursor, coord_user_id):
+  query = """ 
+    SELECT
+	    CONCAT(a.alumno_apellido, ', ', a.alumno_nombre) AS nombre_alumno,
+      p.prestacion_id,
+      p.prestipo_nombre_corto,
+      s.usuario_carga_nombre,
+      s.segalum_rol_carga,     
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 1 THEN 1 ELSE 0 END) AS ene,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 2 THEN 1 ELSE 0 END) AS feb,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 3 THEN 1 ELSE 0 END) AS mar,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 4 THEN 1 ELSE 0 END) AS abr,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 5 THEN 1 ELSE 0 END) AS may,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 6 THEN 1 ELSE 0 END) AS jun,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 7 THEN 1 ELSE 0 END) AS jul,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 8 THEN 1 ELSE 0 END) AS ago,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 9 THEN 1 ELSE 0 END) AS sep,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 10 THEN 1 ELSE 0 END) AS oct,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 11 THEN 1 ELSE 0 END) AS nov,
+      SUM(CASE WHEN MONTH(s.segalum_fec_carga) = 12 THEN 1 ELSE 0 END) AS dic,
+      COUNT(DISTINCT s.segalum_id) AS total_anual
+    FROM
+      v_seguimientos s
+    LEFT JOIN v_prestaciones p
+      ON s.segalum_prestacion = p.prestacion_id
+    JOIN v_alumnos a
+      ON s.segalum_alumno = a.alumno_id
+    WHERE
+      (p.prestacion_estado IN (0, 1) OR p.prestacion_estado IS NULL)
+      AND s.segalum_rol_carga IN ('COORDI', 'EQUIPO_TECNICO')
+      AND (p.prestacion_anio IN (2026, 2027) OR p.prestacion_anio IS NULL)
+      AND YEAR(s.segalum_fec_carga) = 2026
+      AND s.usuario_carga_id = %s
+    GROUP BY
+      p.prestacion_id,
+      a.alumno_id,
+      s.usuario_carga_id,
+      s.segalum_rol_carga
+    ORDER BY
+      nombre_alumno;
+    """
+  cursor.execute(query, (coord_user_id, ))
+  return cursor.fetchall()
--- transform.py
+++ transform.py
@@ -3,7 +3,8 @@ from openpyxl.styles import Font
 from dotenv import load_dotenv
 import os
 import yagmail
-from extract import extract_coordis, extract_pas
+from extract import (extract_coordis, extract_pas, extract_informes, extract_seguim,
+                     extract_seguim_mes)
 
 load_dotenv()
 
@@ -24,7 +25,8 @@ def enviar_mail(destinatario, subject, body, archivo_adjunto):
     print(f"❌ Error enviando mail a {destinatario}: {e}")
 
 
-def generar_excel_por_coordinadora(coord_nombre, pas, coord_mail):
+def generar_excel_por_coordinadora(coord_nombre, pas, informes, seguimientos, seguimientos_mes,
+                                   coord_mail):
   wb = Workbook()
   ws = wb.active
   ws.title = "PAs disponibles"
@@ -38,17 +40,63 @@ def generar_excel_por_coordinadora(coord_nombre, pas, coord_mail):
   for pa in pas:
       ws.append(pa)
 
+# Segunda hoja (informes)
+  ws2 = wb.create_sheet(title="Detalle de informes")
+
+  headers_informes = ["COORDINADORA", "ALUMNO", "DNI ALUMNO", "INF. ADMISIÓN", "CONF. PA", "INF_SOCIAL",
+                      "INF. MENSUAL", "INF. DIAGNÓSTICO", "INF. MEDIO", "PAC", "OTRO", "AA", "PPI", 
+                      "INF. FINAL", "CONF. FLIA.", "INF. ESCOLAR", "INF. TER. EXT.", 
+                      "PLAN TRAB. COORD."]
+  
+  ws2.append(headers_informes)
+
+  for cell in ws2[1]:
+    cell.font = Font(bold=True)
+
+  for row in informes:
+      ws2.append(row)
+
+  # Tercer hoja (seguimientos_detalle)
+  ws3 = wb.create_sheet(title="Detalle de seguimientos")
+
+  headers_seguimientos = ["USUARIO DE CARGA", "ROL", "PRESTACION ID", "TIPO", "ALUMNO", 
+                          "MES", "FECHA DE CARGA", "CATEG. SEGUIMIENTO"]
+  
+  ws3.append(headers_seguimientos)
+
+  for cell in ws3[1]:
+    cell.font = Font(bold=True)
+
+  for row in seguimientos:
+    ws3.append(row)
+
+  # Cuarta hoja (seguimientos_mes)
+  ws4 = wb.create_sheet(title="Seguimientos por mes")
+
+  headers_seguimientos_mes = ["ALUMNO", "PRESTACION ID", "TIPO", "USUARIO DE CARGA", "ROL", "ENE", 
+                              "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEP", "OCT", 
+                              "NOV", "DIC", "TOTAL ANUAL"]
+  
+  ws4.append(headers_seguimientos_mes)
+
+  for cell in ws4[1]:
+    cell.font = Font(bold=True)
+
+  for row in seguimientos_mes:
+    ws4.append(row)
+
   filename = f"reporte_{coord_nombre.replace(', ', '_')}.xlsx"
   wb.save(filename)
   print(f"✅ Excel generado: {filename}")
 
-  # enviar_mail(
-  #   destinatario=coord_mail,
-  #   subject=f"Reporte de PAs disponibles - {coord_nombre} (NO CONTESTAR)",
-  #   body=f"""Hola {coord_nombre},\n\nSe adjunta el listado actualizado de PAs 
-  #     disponibles en tus localidades.\n\nSaludos,\nMariano López - Ailes Inclusión.""",
-  #   archivo_adjunto=filename
-  # )
+  enviar_mail(
+    destinatario=coord_mail,
+    subject=f"Reporte de PAs disponibles - {coord_nombre} (NO CONTESTAR)",
+    body=f"""Hola {coord_nombre},\n\nSe adjunta el listado actualizado de PAs disponibles 
+    en sus localidades junto con el detalle de los informes y seguimientos
+    cargados en Indyco.\n\nSaludos,\nMariano López - Ailes Inclusión.""",
+    archivo_adjunto=filename
+  )
 
 def generar_reportes_por_coordinadora(conn):
   cursor = conn.cursor()
@@ -62,9 +110,14 @@ def generar_reportes_por_coordinadora(conn):
     coord_id = coord[0]
     coord_nombre = coord[1]
     coord_mail = coord[2]
+    coord_user_id = coord[3]
 
     pas = extract_pas(cursor, coord_id)
-    generar_excel_por_coordinadora(coord_nombre, pas, coord_mail)
+    informes = extract_informes(cursor, coord_id)
+    seguimientos = extract_seguim(cursor, coord_id)
+    seguimientos_mes = extract_seguim_mes(cursor, coord_user_id)
+    generar_excel_por_coordinadora(coord_nombre, pas, informes, seguimientos, seguimientos_mes,
+                                   coord_mail)
     cant += 1
     registros += len(pas)
 
